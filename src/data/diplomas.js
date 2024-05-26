046a8cde00384313021c655d39bca9f934e10d59
Luissalamanca23 <user.email like : "l.salamanca18@icloud.com>
2024-05-26 18:56:39 -0400

    first commit

---
+++ src/data/diplomas.js
@@ -0,0 +1,16 @@
+// Import a reference to the image itself
+import EYImagen from "../assets/diplomas/EY/CertificadoEY_HackatonFinXperience.jpg"
+
+const TAGS = {
+    HACKATON: {
+      name: "Hackaton",
+    },
+  };
+  
+  export const DIPLOMAS = [
+    {
+      title: "Hackaton FinXperience 2023 - EY",
+      image: EYImagen,
+      tags: [TAGS.HACKATON],
+    }, 
+  ];
---
+++ src/data/proyects.js
@@ -0,0 +1,16 @@
+import {TAGS} from './tags'
+
+import ECOImage from '../../public/projects/ecocashback.png'
+
+export const PROJECTS = [
+    {
+      title: "EcoCashBack",
+      description:
+        "EcoCashBack is a platform that aims to solve the recycling problem. By purchasing recyclable waste at fair prices, it incentivizes participation. In doing so, we promote a circular economy and raise environmental awareness.",
+      image: ECOImage,
+      github: "https://github.com/Luissalamanca23/EcoCashBack",
+      link: "https:ecocashback.cl",
+      tags: [TAGS.HTML, TAGS.BOOTSTRAP, TAGS.MYSQL],
+    },
+  ]
+  
